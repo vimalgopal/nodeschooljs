0c6d92cfa6259ad055a8f6473d4c3f6e3563c83f
Vimal Gopal <vimal251991@gmail.com>
2018-07-26 09:33:58 +0530

    All nodeschool Projects

---
+++ id-statement.js
@@ -0,0 +1,9 @@
+var fruits;
+fruits='orange'
+len=fruits.lengthto
+
+if (len > 5){
+    console.log("The fruit name has more than five characters.");
+}
+  else
+  {console.log("The fruit name has five characters or less." )}
